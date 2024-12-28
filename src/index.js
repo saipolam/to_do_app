68ee0aa0acc084cad1e47b7d83c5c36b4d96625e
POLAM SAI <147738104+saipolam@users.noreply.github.com>
2024-12-28 07:53:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./to_do_app";
-import "./App.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
